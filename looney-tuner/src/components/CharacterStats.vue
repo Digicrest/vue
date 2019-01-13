7e0e5fe52d1d34e94dbee64ad61e448797e05fc3
Digicrest <kaylaniwalker@gmail.com>
2019-01-13 22:31:19 +0000

    Turned Stat into a component to remove duplication

--- looney-tuner/src/components/CharacterStats.vue
+++ looney-tuner/src/components/CharacterStats.vue
@@ -3,57 +3,22 @@
         <p :style="textColor">{{ name }}</p>
         <hr />
         <div id="stat-container">
-            <div style="display: flex; justify-content: center;">
-                <label style="width: 10%; font-size: .7rem; margin: auto 0;">POW</label>
-                <div class="progress" style="height: 25px; width: 70%;">
-                    <div :style="`width: ${bar_widths.power}%; color: black;`" aria-valuenow="stats.max_power" aria-valuemax="limits.max_power" aria-valuemin="0" role="progressbar" class="progress-bar bg-warning">
-                       {{ stats.max_power }} / {{ limits.max_power }}
-                    </div>
-                </div>
-            </div>
-
-            <div style="display: flex; justify-content: center;">
-                <label style="width: 10%; font-size: .7rem; margin: auto 0;">HP</label>
-                <div class="progress" style="height: 25px; width: 70%;">
-                    <div :style="`width: ${bar_widths.health}%; color: black;`" aria-valuenow="stats.max_health" aria-valuemax="limits.max_health" aria-valuemin="0" role="progressbar" class="progress-bar bg-success">
-                        {{ stats.max_health }} / {{ limits.max_health }}
-                    </div>
-                </div>
-            </div>
-
-
-            <div style="display: flex; justify-content: center;">
-                <label style="width: 10%; font-size: .7rem; margin: auto 0;">ATT</label>
-                <div class="progress" style="height: 25px; width: 70%;">
-                    <div :style="`width: ${bar_widths.attack}%; color: black;`" aria-valuenow="stats.max_attack" aria-valuemax="limits.max_attack" aria-valuemin="0" role="progressbar" class="progress-bar bg-danger">
-                        {{ stats.max_attack }} / {{ limits.max_attack }}
-                    </div>
-                </div>
-            </div>
-
-            <div style="display: flex; justify-content: center;">
-                <label style="width: 10%; font-size: .7rem; margin: auto 0;">DEF</label>
-                <div class="progress" style="height: 25px; width: 70%;">
-                    <div :style="`width: ${bar_widths.defense}%; color: black;`" aria-valuenow="stats.max_defense" aria-valuemax="limits.max_defense" aria-valuemin="0" role="progressbar" class="progress-bar bg-primary">
-                        {{ stats.max_defense }} / {{ limits.max_defense }}
-                    </div>
-                </div>
-            </div>
-
-            <div style="display: flex; justify-content: center;">
-                <label style="width: 10%; font-size: .7rem; margin: auto 0;">SPD</label>
-                <div class="progress" style="height: 25px; width: 70%;">
-                    <div :style="`width: ${bar_widths.speed}%; color: black;`" aria-valuenow="stats.max_speed" aria-valuemax="limits.max_speed" aria-valuemin="0" role="progressbar" class="progress-bar bg-secondary">
-                        {{ stats.max_speed }} / {{ limits.max_speed }}
-                    </div>
-                </div>
-            </div>
+            <character-stat name="POW" bootstrap_class="bg-warning" :width="bar_widths.power" :val="stats.max_power" :limit="limits.max_power"/>
+            <character-stat name="HP" bootstrap_class="bg-success" :width="bar_widths.health" :val="stats.max_health" :limit="limits.max_health"/>
+            <character-stat name="ATT" bootstrap_class="bg-danger" :width="bar_widths.attack" :val="stats.max_attack" :limit="limits.max_attack"/>
+            <character-stat name="DEF" bootstrap_class="bg-primary" :width="bar_widths.defense" :val="stats.max_defense" :limit="limits.max_defense"/>
+            <character-stat name="SPD" bootstrap_class="bg-secondary" :width="bar_widths.speed" :val="stats.max_speed" :limit="limits.max_speed"/>
         </div>
     </div>
 </template>
 
 <script>
+import Stat from "./Stat.vue";
+
 export default {
+    components: {
+        CharacterStat: Stat
+    },
     props: [
         "stats",
         "limits",
@@ -87,9 +52,5 @@ export default {
 </script>
 
 <style scoped>
-    .progress {
-        margin: 5px;
-        /* border: 2px solid silver; */
-        box-shadow: 2px 2px 2px silver;
-    }
+
 </style>
---
+++ looney-tuner/src/components/Stat.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="display: flex; justify-content: center;">
+    <label class="progress_lbl">{{ name }}</label>
+    <div class="progress">
+      <div 
+            :class="`progress-bar ${bootstrap_class}`" 
+            :style="`width: ${width}%; color: black;`" 
+            
+            aria-valuenow="val" 
+            aria-valuemax="limit" 
+            aria-valuemin="0"
+      >{{ val }} / {{ limit }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: [ "width", "val", "limit", "name", "bootstrap_class" ]
+}
+</script>
+
+
+<style scoped>
+    .progress {
+        margin: 5px;
+        box-shadow: 2px 2px 2px silver;
+        height: 25px; 
+        width: 70%;
+    }
+
+    .progress_lbl {
+        width: 10%; 
+        font-size: .7rem; 
+        margin: auto 0;
+    }
+</style>
