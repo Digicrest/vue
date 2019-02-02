90be51512b87d34224820c0d36fb9bc7c2052813
Digicrest <kaylaniwalker@gmail.com>
2019-02-02 16:08:17 +0000

    navigation drawer

--- todo-tracker/src/components/Navbar.vue
+++ todo-tracker/src/components/Navbar.vue
@@ -1,6 +1,9 @@
 <template>
     <nav>
         <v-toolbar app flat>
+            <v-toolbar-side-icon @click="drawer = !drawer" fab color="grey--text">
+                <v-icon>menu</v-icon>
+            </v-toolbar-side-icon>
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>Digicrest</span>
@@ -13,11 +16,19 @@
                 <v-icon right>exit_to_app</v-icon>
             </v-btn>
         </v-toolbar>
+
+        <v-navigation-drawer app v-model="drawer">
+            <p>test</p>
+        </v-navigation-drawer>
     </nav>
 </template>
 
 <script>
 export default {
-    
+    data: function() {
+        return {
+            drawer: false
+        }
+    }
 }
 </script>
