d81ef0a49a01eeb219d40e5e03cd1c0bbe4dcd29
Digicrest <kaylaniwalker@gmail.com>
2019-02-02 18:27:04 +0000

    added sort buttons

---
+++ todo-tracker/src/components/SortButton.vue
@@ -0,0 +1,18 @@
+<template>
+     <v-btn @click="sortBy(type)" small flat color="grey">
+        <v-icon left small>{{ icon }}</v-icon>
+        <span class="caption text-lowercase">{{ text }}</span>
+    </v-btn>
+</template>
+
+<script>
+export default {
+    props: [ "type", "icon", "text" ],
+    methods: {
+        sortBy() {
+            this.$emit("sortBy", this.type)
+        }
+    }
+}
+</script>
+
--- todo-tracker/src/views/Dashboard.vue
+++ todo-tracker/src/views/Dashboard.vue
@@ -3,19 +3,25 @@
     <h1 class="subheading grey--text">Dashboard</h1>
     
     <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <sort-button type="title"  icon="folder" text="by folder name" @sortBy="sortBy"/>
+        <sort-button type="person" icon="person" text="by person" @sortBy="sortBy"/>
+      </v-layout>
       <project-card v-for="project in projects" :key="project.title" :project="project"/>
     </v-container>
   </div>
 </template>
 
 <script>
-import ProjectCard from '@/components/ProjectCard'
+import ProjectCard from '@/components/ProjectCard';
+import SortButton from '@/components/SortButton';
 
 export default {
   components:{
-    ProjectCard
+    ProjectCard,
+    SortButton
   },
-  data(){
+  data: function() {
     return {
        projects: [
         { title: 'Design a new website', person: 'Koshiro Izumi', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
@@ -23,6 +29,11 @@ export default {
         { title: 'Design video thumbnails', person: 'Hikari Yagami', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'T.K.', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    sortBy: function(prop) {
+      this.projects.sort((a, b) => a[prop] < b[prop] ? -1 : 1)
     }
   }
 };
