138a817578a28971e5bf37d3b86eebc748c4e7fe
Digicrest <kaylaniwalker@gmail.com>
2019-02-02 16:13:28 +0000

    theming

--- todo-tracker/src/components/Navbar.vue
+++ todo-tracker/src/components/Navbar.vue
@@ -4,6 +4,7 @@
             <v-toolbar-side-icon @click="drawer = !drawer" fab color="grey--text">
                 <v-icon>menu</v-icon>
             </v-toolbar-side-icon>
+            
             <v-toolbar-title class="text-uppercase grey--text">
                 <span class="font-weight-light">Todo</span>
                 <span>Digicrest</span>
@@ -17,8 +18,8 @@
             </v-btn>
         </v-toolbar>
 
-        <v-navigation-drawer app v-model="drawer">
-            <p>test</p>
+        <v-navigation-drawer app v-model="drawer" class="primary">
+            
         </v-navigation-drawer>
     </nav>
 </template>
--- todo-tracker/src/plugins/vuetify.js
+++ todo-tracker/src/plugins/vuetify.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 
-Vue.use(Vuetify, {
+const vuetify_settings = {
   iconfont: 'md',
-})
+  theme: {
+    primary:  '#9652FF',
+    success:  '#3CD1C2',
+    info:     '#FFAA2C',
+    error:    '#F83E70'
+  }
+};
+
+Vue.use(Vuetify, vuetify_settings)
