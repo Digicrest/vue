cf091aa8ab18085ace1510838f4406c66827aa18
Digicrest <kaylaniwalker@gmail.com>
2019-02-02 20:10:04 +0000

    form input validation

--- todo-tracker/src/components/Popup.vue
+++ todo-tracker/src/components/Popup.vue
@@ -7,17 +7,17 @@
             </v-card-title>
 
             <v-card-text>
-                <v-form class="px-3">
-                    <v-text-field label="Title" v-model="title" prepend-icon="folder"></v-text-field>
-                    <v-textarea label="Information" v-model="content" prepend-icon="edit"></v-textarea>
+                <v-form class="px-3" ref="form">
+                    <v-text-field label="Title" v-model="title" prepend-icon="folder" :rules="inputRules"></v-text-field>
+                    <v-textarea label="Information" v-model="content" prepend-icon="edit" :rules="inputRules"></v-textarea>
 
                     <v-menu>
-                        <v-text-field :value="formattedDate" slot="activator" label="Due Date" prepend-icon="date_range"></v-text-field>
+                        <v-text-field :rules="inputRules" :value="formattedDate" slot="activator" label="Due Date" prepend-icon="date_range"></v-text-field>
                         <v-date-picker v-model="due"></v-date-picker>
                     </v-menu>
 
                     <v-spacer></v-spacer>
-                    
+
                     <v-btn flat class="success mx-0 mt-3" @click="submit">Add Project</v-btn>
                 </v-form>
             </v-card-text>
@@ -33,12 +33,17 @@ export default {
         return {
             title: "",
             content: "",
-            due: null
+            due: null,
+            inputRules: [
+                v => v && v.length >= 3 || 'Minimum Length is 3 Characters'
+            ]
         }
     },
     methods: {
         submit() {
-            console.log(this.title, this.content)
+            if(this.$refs.form.validate()) {
+                console.log(this.title, this.content)
+            }
         }
     },
     computed: {
