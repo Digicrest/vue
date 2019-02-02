5430f41a876c349cb8e2000d19c9f8f09aa7f514
Digicrest <kaylaniwalker@gmail.com>
2019-02-02 15:58:28 +0000

    basic toolbar

--- todo-tracker/src/App.vue
+++ todo-tracker/src/App.vue
@@ -1,14 +1,20 @@
 <template>
-  <v-app>
+  <v-app class="grey lighten-4">
     <v-content>
+      <navbar/>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar";
+
 export default {
   name: "App",
+  components: {
+    Navbar
+  },
   data() {
     return {
       //
--- todo-tracker/src/views/Home.vue
+++ todo-tracker/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    
+    
   </div>
 </template>
 
